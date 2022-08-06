cfbcb9f4b4ff93aa87581ba6df02e1cf988825a7
Snehal Mane <snehal6mane@gmail.com>
2022-08-06 22:36:22 +0530

    check capacity route added

--- routes/order.js
+++ routes/order.js
@@ -3,11 +3,52 @@ const router = express.Router();
 const { isValidObjectId } = require("mongoose");
 
 const Order = require('../models/Order');
-const { orderValidation, editValidation } = require("../utils/dataValidation")
+const { orderValidation, editValidation, dateValidation } = require("../utils/dataValidation")
 
 // @route   GET /api/checkCapacity/:date
 // @desc    Return left milk for the day
 // @access  Public
+router.get("/checkCapacity/:date", dateValidation, async (req, res) => {
+  try {
+    // dateValidation sets req.formattedDate in "mm/dd/yyyy" format
+    // We get date in the format "dd/mm/yyyy" in query parameters
+
+    // We set a queryDate equal to the date for which we have to check the milk left
+    // nextDate is the day after queryDate
+    const queryDate = new Date(req.formattedDate), nextDate = new Date(req.formattedDate);
+    nextDate.setDate(nextDate.getDate() + 1);
+
+    // Find orders which lie between queryDate and nextDate
+    // Select only the ordered quantity
+    const orderedMilkData = await Order.find({
+      orderedAt: {
+        $gte: queryDate,
+        $lt: nextDate
+      }
+    }).select("quantity -_id");
+
+    // We get an array of objects from the query above
+    // Sum all of the quantities
+    var orderedMilk = 0;
+    orderedMilkData.forEach((data) => orderedMilk += data.quantity);
+
+    // Subtract total ordered milk quantity from the max capacity
+    // Max capacity is currently defined in environment variables but can also be saved in database
+    const maxCapacity = process.env.MAX_CAPACITY;
+
+    // Return the milk day for the queried date
+    res.status(200).json({
+      data: {
+        milkLeft: maxCapacity - orderedMilk
+      }
+    })
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
+  }
+})
 
 // @route   GET /api/:orderId
 // @desc    Get order by id
@@ -20,6 +61,7 @@ router.get("/order/:orderId", async (req, res) => {
     if (orderId === undefined || orderId.length === 0 || !isValidObjectId(orderId)) return res.status(400).json({ error: "Invalid order ID" });
 
     const orderData = await Order.findById(orderId);
+    // orderData is null => order having _id == orderId is not present in the database
     if (!orderData) return res.status(404).json({ error: "Order not found" });
 
     res.status(200).json({
@@ -61,6 +103,7 @@ router.post("/add", orderValidation, async (req, res) => {
       customerId, deliveryAddress, quantity, status
     });
 
+    // Save the document and get the newly saved document to return
     const savedOrder = await order.save();
 
     res.status(201).json({ data: savedOrder });
@@ -82,13 +125,16 @@ router.put("/updateStatus/:orderId", async (req, res) => {
     // Check if order id is present and a valid mongodb id
     if (orderId === undefined || orderId.length === 0 || !isValidObjectId(orderId)) return res.status(400).json({ error: "Invalid order ID" });
 
+    // Status should belong to enum values
     const { status } = req.body;
     if (!status || status.length === 0 || !["placed", "packed", "dispatched", "delivered"].includes(status)) return res.status(400).json({ error: "Invalid order status" });
 
     const order = await Order.findById(orderId);
     if (!order) return res.status(404).json({ error: "Order not found" });
 
+    // Update order status value
     order.status = status;
+    // Save order document and return newly updated document
     const updatedOrder = await order.save();
     res.status(200).json({ data: updatedOrder });
   } catch (err) {
@@ -111,11 +157,13 @@ router.put("/:orderId", editValidation, async (req, res) => {
 
     const { customerId, deliveryAddress, quantity, status } = req.body;
 
+    // Set the fields passed in the request body and return the updated document
     const orderData = await Order.findByIdAndUpdate(orderId, {
       $set: {
         customerId, deliveryAddress, quantity, status
       }
     }, { new: true });
+    // If orderData is null, that means order with _id == orderId is not present in the database
     if (!orderData) return res.status(404).json({ error: "Order not found" });
 
     res.status(200).json({ data: orderData });
--- utils/dataValidation.js
+++ utils/dataValidation.js
@@ -7,17 +7,16 @@ const orderValidation = (req, res, next) => {
   const errors = [];
 
   if (!customerId || customerId.length === 0) errors.push("Invalid customer ID");
-  // if (!customerId || customerId.length === 0 || !isValidObjectId(customerId)) errors.push("Invalid customer ID");
 
   if (!deliveryAddress) errors.push("Delivery address required");
 
+  // If deliveryAddress is present in the req.body, check its components
   if (deliveryAddress && (!deliveryAddress.address || deliveryAddress.address.length === 0)) errors.push("Address required");
 
   if (deliveryAddress && (!deliveryAddress.city || deliveryAddress.city.length === 0)) errors.push("City required");
 
+  // TODO check if state belongs to enum
   if (deliveryAddress && (!deliveryAddress.state || deliveryAddress.state.length === 0)) errors.push("State required");
-
-  // Check that state belongs to enum
 
   if (deliveryAddress && (deliveryAddress.pincode === undefined || !Number.isInteger(Number(deliveryAddress.pincode)) || Number(deliveryAddress.pincode) < 0)) errors.push("Invalid pincode");
 
@@ -57,4 +56,32 @@ const editValidation = (req, res, next) => {
   else next();
 }
 
-module.exports = { orderValidation, editValidation }
+const dateValidation = (req, res, next) => {
+  // Format of date is dd-mm-yyyy
+  const { date } = req.params;
+
+  // If date is not passed
+  if (!date || date.length === 0) return res.status(400).json({ error: "Date is required" });
+
+  // Minimum length of date should be 8 and max should be 10
+  // It should match the pattern of dd-mm-yyyy (single digit days and months also work)
+  if (date.length < 8 || date.length > 10 || !date.match(/[0-9]{1,2}[-]{1}[0-9]{1,2}[-]{1}[0-9]{4}/)) return res.status(400).json({ error: "Invalid date" });
+
+  // The date format is valid
+  // We split the date into day, month and year components
+  // Convert to the format of mm/dd/yyyy
+  const dateArray = date.split("-");
+  const day = dateArray[0];
+  const month = dateArray[1];
+  const year = dateArray[2];
+  const formattedDate = `${month}/${day}/${year}`;
+
+  // Check if formatted date is valid
+  if (isNaN(Date.parse(formattedDate))) return res.status(400).json({ error: "Invalid date" });
+
+  // The formatted date is valid, set it in the request object and return to the main function
+  req.formattedDate = formattedDate;
+  next();
+}
+
+module.exports = { orderValidation, editValidation, dateValidation }
