25d9ac1f1305625b89238d04655b48dfddd2c5af
Snehal Mane <snehal6mane@gmail.com>
2022-08-06 19:52:37 +0530

    Resolved mongo auth error

--- config/database.js
+++ config/database.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose")
 
 const connectToDatabase = async () => {
   try {
-    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.o2jpvo2.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = process.env.DB_URI;
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("Database connected");
   } catch (err) {
+    console.error(err);
     console.error(err.message);
     process.exit(1);
   }
