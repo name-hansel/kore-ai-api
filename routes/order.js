4245ab5e1110b2cf86569525e6fe45799f6f01f2
Snehal Mane <snehal6mane@gmail.com>
2022-08-06 21:38:59 +0530

    Update routes added

--- routes/order.js
+++ routes/order.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const { isValidObjectId } = require("mongoose");
 
 const Order = require('../models/Order');
-const { orderValidation } = require("../utils/dataValidation")
+const { orderValidation, editValidation } = require("../utils/dataValidation")
+
+// @route   GET /api/checkCapacity/:date
+// @desc    Return left milk for the day
+// @access  Public
 
 // @route   GET /api/:orderId
 // @desc    Get order by id
 // @access  Public
-router.get("/:orderId", async (req, res) => {
+router.get("/order/:orderId", async (req, res) => {
   try {
     const orderId = req.params.orderId;
 
@@ -32,7 +36,7 @@ router.get("/:orderId", async (req, res) => {
 // @route   GET /api
 // @desc    Get all orders
 // @access  Public
-router.get("/", async (req, res) => {
+router.get("/order", async (req, res) => {
   try {
     const orderData = await Order.find();
     res.status(200).json({
@@ -60,6 +64,61 @@ router.post("/add", orderValidation, async (req, res) => {
     const savedOrder = await order.save();
 
     res.status(201).json({ data: savedOrder });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
+  }
+})
+
+// @route   PUT /api/updateStatus/:orderId
+// @desc    Update order status
+// @access  Public
+router.put("/updateStatus/:orderId", async (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+
+    // Check if order id is present and a valid mongodb id
+    if (orderId === undefined || orderId.length === 0 || !isValidObjectId(orderId)) return res.status(400).json({ error: "Invalid order ID" });
+
+    const { status } = req.body;
+    if (!status || status.length === 0 || !["placed", "packed", "dispatched", "delivered"].includes(status)) return res.status(400).json({ error: "Invalid order status" });
+
+    const order = await Order.findById(orderId);
+    if (!order) return res.status(404).json({ error: "Order not found" });
+
+    order.status = status;
+    const updatedOrder = await order.save();
+    res.status(200).json({ data: updatedOrder });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
+  }
+})
+
+// @route   PUT /api/:orderId
+// @desc    Update order
+// @access  Public
+router.put("/:orderId", editValidation, async (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+
+    // Check if order id is present and a valid mongodb id
+    if (orderId === undefined || orderId.length === 0 || !isValidObjectId(orderId)) return res.status(400).json({ error: "Invalid order ID" });
+
+    const { customerId, deliveryAddress, quantity, status } = req.body;
+
+    const orderData = await Order.findByIdAndUpdate(orderId, {
+      $set: {
+        customerId, deliveryAddress, quantity, status
+      }
+    }, { new: true });
+    if (!orderData) return res.status(404).json({ error: "Order not found" });
+
+    res.status(200).json({ data: orderData });
   } catch (err) {
     console.error(err.message);
     return res.status(500).json({
--- utils/dataValidation.js
+++ utils/dataValidation.js
@@ -23,8 +23,38 @@ const orderValidation = (req, res, next) => {
 
   if (quantity === undefined || quantity < 0) errors.push("Invalid quantity");
 
+  // If status is given in request body, validate it
+  // Or default value of "placed" is used in DB
+  if (status && (status.length === 0 || !["placed", "packed", "dispatched", "delivered"].includes(status))) errors.push("Invalid status");
+
   if (errors.length > 0) return res.status(400).json({ errors });
   else next();
 }
 
-module.exports = { orderValidation }
+const editValidation = (req, res, next) => {
+  const { deliveryAddress, quantity, status } = req.body;
+  const errors = [];
+
+  if (!deliveryAddress) errors.push("Delivery address required");
+
+  if (deliveryAddress && (!deliveryAddress.address || deliveryAddress.address.length === 0)) errors.push("Address required");
+
+  if (deliveryAddress && (!deliveryAddress.city || deliveryAddress.city.length === 0)) errors.push("City required");
+
+  if (deliveryAddress && (!deliveryAddress.state || deliveryAddress.state.length === 0)) errors.push("State required");
+
+  // Check that state belongs to enum
+
+  if (deliveryAddress && (deliveryAddress.pincode === undefined || !Number.isInteger(Number(deliveryAddress.pincode)) || Number(deliveryAddress.pincode) < 0)) errors.push("Invalid pincode");
+
+  if (quantity === undefined || quantity < 0) errors.push("Invalid quantity");
+
+  // If status is given in request body, validate it
+  // Or default value of "placed" is used in DB
+  if (status && (status.length === 0 || !["placed", "packed", "dispatched", "delivered"].includes(status))) errors.push("Invalid status");
+
+  if (errors.length > 0) return res.status(400).json({ errors });
+  else next();
+}
+
+module.exports = { orderValidation, editValidation }
